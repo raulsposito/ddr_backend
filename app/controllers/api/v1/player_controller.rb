9fe1282343dfb233cc85da558204cdb38324dee6
raulsposito <raulsposito@gmail.com>
2020-12-16 20:57:10 -0300

    fix controller to render json

--- app/controllers/api/v1/player_controller.rb
+++ app/controllers/api/v1/player_controller.rb
@@ -6,16 +6,12 @@ class Api::V1::PlayerController < ApplicationController
 
     def index
         players = Player.all
-        render json: players.to_json(:include => {
-            :name
-        })
+        render json: players
     end
 
     def show
         player = Player.find_by(id: params[:id])
-        render json: player.to_json(:include => {
-            :name
-        })
+        render json: player
     end
 
 end
